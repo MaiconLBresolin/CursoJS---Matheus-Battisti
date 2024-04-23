593561052b5aff5a60d84b6287103b5cd97e36d7
Maicon Luís Bresolin <maiconbresolin66@gmail.com>
2024-04-23 12:41:03 -0300

    feat: adding Exercicio_1

---
+++ Seção_3/Exercicio_1/script.js
@@ -0,0 +1,7 @@
+let nome1 = "Maicon";
+let nome2 = "Luis";
+let nome3 = "Bresolin";
+
+console.log("Meu nome é: " + nome1);
+console.log("Meu nome do meio é: " + nome2);
+console.log(`Meu sobrenome é: ${nome3}`);
