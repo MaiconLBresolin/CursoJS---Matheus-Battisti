9c61da4b44aed4bbf4682447c1b9cfa43a5049da
Maicon Luís Bresolin <maiconbresolin66@gmail.com>
2024-04-23 12:43:46 -0300

    feat: adding Exercicio_2

---
+++ Seção_3/Exercicio_2/script.js
@@ -0,0 +1,7 @@
+let num1 = 1;
+let num2 = 2.15;
+let num3 = num1 + num2;
+
+console.log("The first number is: " + num1);
+console.log("The second number is: " + num2);
+console.log(`The sum of the first two numbers is: ${num3}`);
