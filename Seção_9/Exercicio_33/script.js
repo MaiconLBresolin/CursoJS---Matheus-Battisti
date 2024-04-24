f07c00bae3325ec1d72d04ea95f96406dc01df84
Maicon Luís Bresolin <maiconbresolin66@gmail.com>
2024-04-24 17:22:32 -0300

    feat: adding Exercicio_33

---
+++ Seção_9/Exercicio_33/script.js
@@ -0,0 +1,10 @@
+/*let carro = {
+  "marca" : "toyota",
+  "motor" : "1.8",
+  "ano" : 2021
+};*/
+//prettier is not allowing the json simulation in js to work, but the format is right
+
+console.log(carro.marca);
+console.log(carro.motor);
+console.log(carro.ano);
