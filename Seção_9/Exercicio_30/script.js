95c1386a41510967a8efbe36ccb7ad02551eb98b
Maicon Luís Bresolin <maiconbresolin66@gmail.com>
2024-04-24 16:58:34 -0300

    feat: adding Exercicio_30

---
+++ Seção_9/Exercicio_30/script.js
@@ -0,0 +1,7 @@
+let names = ["Maicon", "Bruna", "Gabriela", "Severino", "Eduarda"];
+
+if (names.includes("Maicon") == true) {
+  console.log("O nome está presente");
+} else {
+  console.log("Não está presente");
+}
