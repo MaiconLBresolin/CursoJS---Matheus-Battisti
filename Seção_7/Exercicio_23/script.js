19d51224577bc3f35841072e22f709aa35cf41b4
Maicon Luís Bresolin <maiconbresolin66@gmail.com>
2024-04-24 14:03:15 -0300

    feat: adding Exercicio_23

---
+++ Seção_7/Exercicio_23/script.js
@@ -0,0 +1,11 @@
+function tamString(frase) {
+  if (frase.lenght > 10) {
+    console.log("Texto muito longo");
+  } else {
+    console.log("Texto dentro do limite");
+  }
+}
+
+tamString("Ola meu nome é Maicon");
+tamString("Ola");
+tamString("1234567890");
